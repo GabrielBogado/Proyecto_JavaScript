e7109330b4b2fdc367922402491d7aa82ee7330f
Gabriel Bogado <bogado.gabriel141@gmail.com>
2022-09-27 20:40:41 -0300

    mejorando-codigo

--- js/pre-entrega.js
+++ js/pre-entrega.js
@@ -22,12 +22,28 @@ function opciones(){
     let opcion = parseInt(prompt("Ingrese una opcion: \n1) Cervezas \n2) Gaseosas \n3) Vinos \n4) Vodka \n5) Salir "))
     if(opcion>=1 && opcion<=4){
         console.log("Usted selecciono la opcion:", opcion)
-        menu()
+        menu(opcion)
     }else{
         console.log("Gracias por su visita")
     }
 }
 
-function menu(){
-    console.log("Aca va el menu en un futuro")
+function menu(opcion){
+    switch(opcion){
+        case 1:
+            console.log("Aqui tiene la variedad de Cervezas")
+            break
+        case 2:
+            console.log("Aqui tiene la variedad de Gaseosas")
+            break
+        case 3:
+            console.log("Aqui tiene la variedad de Vinos")
+            break
+        case 4:
+            console.log("Aqui tiene la variedad de Vodka")
+            break
+        default:
+            console.error("No hay seleccion")
+            break
+    }
 }
